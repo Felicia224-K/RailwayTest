70ad0a8baee82db1b86930508265a6d4d2558243
Mamy Lainé Soumaoro <lainemamysoumaoro@gmail.com>
2026-03-23 22:36:54 +0000

    Fix: All

--- src/config/database.js
+++ src/config/database.js
@@ -1,12 +1,16 @@
 
 const {Sequelize} = require("sequelize");
 
+if (!process.env.DATABASE_URL) {
+    throw new Error("DARABASE_URL is not set");
+}
+
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: "postgres",
     logging: process.env.NODE_ENV === "development" ? console.log : false,
     dialectOptions: {
         ssl: process.env.NODE_ENV === "production"
-        ? { require: true, rejectunauthorized: false}
+        ? { require: true, rejectUnauthorized: false}
         : false,
     },
 });
